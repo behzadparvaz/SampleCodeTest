29bad31b4d5d8c92b1fcffc190d5c2b5ce476574
Parvaz.Behzad <Parvaz.Behzad@okco.ir>
2022-11-22 12:58:06 +0330

    init project

---
+++ ConsoleAppSample/ConsoleAppSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleAppSample;
+using System;
+
+SampleCode sample = new SampleCode();
+await sample.Print("Hello");
+await sample.Print("world");
+await sample.Print("Ali");
+
+
+
+///// لطفا نمونه بنویسید که خروجی بالا به صورت Async
+
+
+
+
+//// لطفا نمونه بنویسید که خروجی بالا به صورت موازی اجرا شود
---
+++ ConsoleAppSample/ConsoleAppSample/SampleCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSample
+{
+    public class SampleCode
+    {
+        public Task Print(string message)
+        {
+           return Task.Run(() => {
+                Task.Delay(5000);
+                Console.WriteLine(message);
+            });
+        }
+    }
+}
