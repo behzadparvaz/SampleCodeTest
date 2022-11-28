af78c55964cfcc0b5ebba93e271f98e00876a9aa
namavaran <namavaran.xyz@gmail.com>
2022-11-28 10:11:29 +0330

    feed(APM) : add async programming model
    add async programming model to run async tasks and run tasks parallel

--- ConsoleAppSample/ConsoleAppSample/Program.cs
+++ ConsoleAppSample/ConsoleAppSample/Program.cs
@@ -1,16 +1,19 @@
 ﻿using ConsoleAppSample;
 using System;
+using System.Threading.Tasks;
 
 SampleCode sample = new SampleCode();
-await sample.Print("Hello");
-await sample.Print("world");
-await sample.Print("Ali");
+
 
 
 
 ///// لطفا نمونه بنویسید که خروجی بالا به صورت Async
 
-
+await sample.PrintAsync("Hello");
+await sample.PrintAsync("world");
+await sample.PrintAsync("Ali");
 
 
 //// لطفا نمونه بنویسید که خروجی بالا به صورت موازی اجرا شود
+
+await Task.WhenAll(sample.PrintAsync("Hello"), sample.PrintAsync("world"), sample.PrintAsync("Ali"));
